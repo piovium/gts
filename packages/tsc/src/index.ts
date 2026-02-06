4329acff11844304c907858ae36c33f045535144
Guyutongxue <guyutongxue@163.com>
2026-02-06 14:32:28 +0800

    fixed
    Change-Id: I903a5a1845f47c32ea52b6c69318f3ccd99683db

--- packages/tsc/src/index.ts
+++ packages/tsc/src/index.ts
@@ -1,4 +1,4 @@
-import { runTsc } from "@volar/typescript/lib/quickstart/runTsc";
+import { runTsc } from "@volar/typescript/lib/quickstart/runTsc.js";
 import { createGtsLanguagePlugin } from "@gi-tcg/gts-language-plugin";
 import { createRequire } from "node:module";
 
@@ -6,6 +6,5 @@ const require = createRequire(import.meta.url);
 const tscPath = require.resolve("typescript/lib/tsc");
 runTsc(tscPath, [".gts"], (ts, options) => {
   const gtsLanguagePlugin = createGtsLanguagePlugin(ts);
-  console.log(gtsLanguagePlugin);
   return { languagePlugins: [gtsLanguagePlugin] };
 });
