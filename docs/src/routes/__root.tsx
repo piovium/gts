89776d0019dc14f0a6869c8a04396cc527cf2303
Guyutongxue <guyutongxue@163.com>
2026-02-18 19:51:10 +0800

    change docs title

--- docs/src/routes/__root.tsx
+++ docs/src/routes/__root.tsx
@@ -14,7 +14,7 @@ export const Route = createRootRoute({
         content: 'width=device-width, initial-scale=1',
       },
       {
-        title: 'Fumadocs on TanStack Start',
+        title: 'GamingTS Documentation',
       },
     ],
     links: [{ rel: 'stylesheet', href: appCss }],
